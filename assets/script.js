66cb2f806c229b1234bb9ab8e1b2c341fea9e43a
Luke <lukeconrad.stew@gmail.com>
2021-11-29 22:46:46 -0800

    game starts with questions

--- assets/script.js
+++ assets/script.js
@@ -1,51 +1,51 @@
 // console.log ("sup");
-
+var quizContent = document.querySelector(".quiz-content");
+var gameContent = document.querySelector(".game");
 var startBtn = document.querySelector(".start-btn");
 var correctAnswer;
 var timer;
 var time = 75;
+var answer; 
+var gameContent;
 
-
+var questionIndex = 0;
 var questionsArray = [
     {
         question: "How do you make final changes to your local git repository?",
-        a: "git add",
-        b: "git commit",
-        c: "git pull",
-        d: "git push",
+        answers: ["git add", "git commit", "git pull", "git push"],
         correctAnswer: "d"
     },
     {
         question: "What does HTML stand for?",
-        a: "Hyper Text Main Language",
-        b: "Hyper Text Markup Language",
-        c: "Hip Tools Making Language",
-        d: "Hot Terminal Machine Language",
+        answers: ["Hyper Text Main Language", "Hyper Text Markup Language", "Hip Tools Making Language", "Hot Terminal Machine Language"],
         correctAnswer: "b",
     },
     {
         question: "How many ways are there to style a page?",
-        a: "endless",
-        b: "1",
-        c: "5",
-        d: "10 or more",
+        answers: ["endless", "1", "5", "10 or more"],
         correctAnswer: "a",
     },
     {
         question: "What does DOM stand for?",
-        a: "Demanding Obects More",
-        b: "Document Object Model",
-        c: "Document Orgin Master",
-        d: "Docment Oringal Main",
+        answers: ["Demanding Obects More", "Document Object Model", "Document Orgin Master", "Docment Oringal Main"],
         correctAnswer: "b",
     },
 ];
+       
+        
+      
+       
 
 
 function startGame() {
     startTimer()
-    var quizContent = document.querySelector(".quiz-content");
+  
     quizContent.classList.add("hidden");
+    gameContent.classList.remove("hidden");
+    displayQuestions ();
+}
+function displayQuestion() {
+    gameContent.textContent =
 }
 
 function startTimer() {
@@ -57,6 +57,8 @@ function startTimer() {
             gameOver()
         }
     }, 1000);
+
+
 
 
 }
